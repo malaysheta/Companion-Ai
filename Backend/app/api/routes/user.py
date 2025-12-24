bef858ea9e23ff545914eb73a23a8a3434a37c90
malaysheta <152248884+malaysheta@users.noreply.github.com>
2025-12-24 23:30:24 +0530

    Implement user authentication with OAuth2 and update login functionality with help of rishi and under the guidances of Dr.Jenil Sutariya

--- Backend/app/api/routes/user.py
+++ Backend/app/api/routes/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
 from bson import ObjectId
 from typing import List
 from app.core.responce import ApiResponse
@@ -7,9 +7,10 @@ from app.database import mongoDb
 from app.utils.hashing import Hash
 from app.utils.JWTtoken import create_access_token
 from dotenv import load_dotenv
+from app.utils.oauth2 import get_current_user
 import os
+from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
-
 load_dotenv()
 
 
@@ -46,13 +47,13 @@ async def create_user(request : userSchema.CreateUser):
 
 
 @router.post('/login',response_model=dict)
-async def login(request : userSchema.login):
+async def login(request: OAuth2PasswordRequestForm = Depends()):
     collection = mongoDb.get_user_collection()
 
     if not collection:
         return ApiResponse.error(message="User collection not found",status_code=500,errors="Internal server error")
     
-    email = request.email.strip().lower()
+    email = request.username.strip().lower()
     user = collection.find_one({"email" : email})
     print("user :" ,user)
 
@@ -72,11 +73,15 @@ async def login(request : userSchema.login):
     data={"sub": user["email"]},
     expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     )
+    data={
+        "access_token":access_token,
+        "token_type":"bearer"
+    }
 
-    return ApiResponse.success(data=access_token,message="Login successfully",status_code=200)
+    return data
 
 @router.get("/user_data",response_model=dict)
-async def user():
+async def user(x:dict=Depends(get_current_user)):
     collection = mongoDb.get_user_collection()
     users=collection.find({})
     users_list=[]
@@ -86,5 +91,5 @@ async def user():
             "email":user["email"]
         }
         users_list.append(temp)
-    return ApiResponse.success(data=users_list,status_code=200,message="user details are fetched")
+    return ApiResponse.success(data=str(users_list),status_code=200,message="user details are fetched")
     
---
+++ Backend/app/utils/oauth2.py
@@ -0,0 +1,26 @@
+from fastapi import Depends,HTTPException,status
+
+from fastapi.security import OAuth2PasswordBearer
+from . import JWTtoken
+from app.database.mongoDb import get_user_collection
+from pydantic import BaseModel
+
+class auth_token(BaseModel):
+    token:str
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/", auto_error=True)
+
+async def get_current_user(token:str=Depends(oauth2_scheme)):
+     credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+     
+     user_decode=JWTtoken.verifyJWT(token=token)
+     print(user_decode)
+
+     if not user_decode:
+          raise credentials_exception
+     
+     return user_decode

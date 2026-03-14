306ea4173cba2da0c5ef66fb576134eea16daff0
malaysheta <152248884+malaysheta@users.noreply.github.com>
2026-03-14 13:43:22 +0530

    feat: Initialize FastAPI application with core setup, routes, CORS, and MongoDB integration.

--- Backend/main.py
+++ Backend/main.py
@@ -5,9 +5,27 @@ from app.api.routes.user import router as user_router
 from app.api.routes.upload import router as manual_router
 from app.api.routes.manuals import router as manual_retrival_router
 from app.core.responce import ApiError
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="Companion AI Backend")
+
+# Define allowed origins for CORS
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://companion-ai-iota.vercel.app",
+    "https://companion-ai.vercel.app",
+]
+
+# Add CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(ApiError)
 async def api_error_handler(request: Request, exc: ApiError):
